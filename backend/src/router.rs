b5f5d89c041d9c5a7049654b54efc3c122a7a956
Justin Leahy <justinjleahy@gmail.com>
2025-05-31 15:16:33 -0400

    Implemented health check endpoint

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,24 +1,33 @@
-use axum::{
-    Router
-};
+use axum::{Json, Router};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use axum::routing::get;
-use utoipa::OpenApi;
+use chrono::Utc;
+use utoipa::{OpenApi, ToSchema};
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_axum::routes;
 use utoipa_swagger_ui::SwaggerUi;
+use serde::Serialize;
+
+#[derive(Serialize, ToSchema)]
+struct Health {
+    #[serde(rename = "CurrentTime")]
+    current_time: String,
+}
 
 #[utoipa::path(
     get,
-    path = "/home",
-    tag = "Home",
+    path = "/api/health",
+    tag = "Health",
     responses(
-        (status = 200, description = "Displays the home page")
+        (status = 200, description = "Returns server's current time and status for basic availability monitoring.", body = Health)
     )
 )]
-async fn home() -> impl IntoResponse {
-    (StatusCode::OK, "home")
+pub async fn health() -> impl IntoResponse {
+    let health = Health {
+        current_time: Utc::now().to_rfc3339()
+    };
+
+    (StatusCode::OK, Json(health))
 }
 
 pub fn create_router() -> Router {
@@ -27,12 +36,11 @@ pub fn create_router() -> Router {
     struct ApiDoc;
 
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
-        .routes(routes!(home))
+        .routes(routes!(health))
         .split_for_parts();
 
     let router = router
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api.clone()))
-        .route("/", get(home));
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api.clone()));
 
     return router;
 }
