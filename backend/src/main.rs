9b64e01a06eb834599e8a007330b5774695e30c4
Justin Leahy <justinjleahy@gmail.com>
2025-05-31 15:55:17 -0400

    Implemented logging into the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,20 +3,36 @@ mod router;
 use std::io::Error;
 use std::net::{ Ipv4Addr, SocketAddr };
 use tokio::net::TcpListener;
+use tower_http::trace::{self, TraceLayer};
 use tower_http::cors::{Any, CorsLayer};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods(Any)
         .allow_headers(Any);
 
     let router = router::create_router()
-        .layer(cors);
+        .layer(cors)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new()
+                    .level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new()
+                    .level(Level::INFO)),
+        );
+
 
     let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8080));
     let listener = TcpListener::bind(&address).await?;
+    tracing::info!("Listening on {}", address);
     axum::serve(listener, router.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
         .await
